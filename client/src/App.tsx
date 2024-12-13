d6a7418dde6dd0d16cb7b112e5962d68794d58a8
NazmulHasan68 <aishadowzone@gmail.com>
2024-12-13 16:50:04 +0600

    Add react-dom and complete login ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,36 @@
-
 import './App.css'
-import { Button } from './components/ui/button'
+import Login from './auth/Login'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import MainLayout from './MainLayout'
+import Signup from './auth/Signup'
 
+const appRouter = createBrowserRouter([
+  {
+    path : '/',
+    element : <MainLayout/>,
+    children:[
+      // {
+      //   path 
+      // }
+    ]
+  },
+  {
+    path : '/login',
+    element : <Login/>
+  },
+  {
+    path : '/signup',
+    element : <Signup/>
+  }
+])
 function App() {
 
   return (
-    <Button variant="destructive">Destructive</Button>
+    <main className=''>
+      <RouterProvider router={appRouter}>
+          
+      </RouterProvider>
+    </main>
   )
 }
 
---
+++ client/src/auth/Login.tsx
@@ -0,0 +1,93 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Loader2, LockKeyhole, Mail } from "lucide-react";
+import React, { FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
+
+
+// typescript me type define krne ka 2 type hotahe 
+interface LoginInputState {
+    email : string,
+    password: string,
+}
+
+
+const Login = () => {
+    const [input, setinput] = useState<LoginInputState>({
+        email : "",
+        password : ""
+    })
+    const changeEventHandler = (e:ChangeEvent<HTMLInputElement>) =>{
+        const {name , value} = e.target;
+        setinput({...input, [name]:value})
+    }
+    const loginSubmitHandler = (e:FormEvent)=>{
+        e.preventDefault()
+        console.log(input);
+        
+    }
+    const loading = false;
+
+  return (
+    <div className="flex items-center justify-center min-h-screen w-full">
+      <form onSubmit={loginSubmitHandler} className="md:p-8 w-full md:max-w-md md:border md:border-gray-200 rounded-lg mx-4">
+        <div className="mb-4">
+          <h1 className="font-bold text-2xl">FoodShadow</h1>
+        </div>
+        <div className="mb-4">
+          <div className="relative">
+            <label htmlFor="email" className="sr-only">Email</label>
+            <Input
+              id="email"
+              type="email"
+              name='email'
+              value={input.email}
+              onChange={changeEventHandler}
+              placeholder="Email"
+              className="pl-10 focus:outline-none"
+            />
+            <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+        <div className="mb-4">
+          <div className="relative">
+            <label htmlFor="password" className="sr-only">Password</label>
+            <Input
+              id="password"
+              type="password"
+              name="password"
+              value={input.password}
+              onChange={changeEventHandler}
+              placeholder="Password"
+              className="pl-10 focus:outline-none"
+            />
+            <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+          </div>
+        </div>
+        <div className="mb-4">
+          <Button
+            type="submit"
+            className={`w-full ${loading ? 'bg-gray-500 cursor-not-allowed' : 'bg-orange hover:bg-hoverOrange'}`}
+            disabled={loading}
+          >
+            {loading ? (
+              <Loader2 className="mr-1 h-4 w-4 animate-spin" />
+            ) : (
+              "Login"
+            )}
+          </Button>
+        </div>
+        <Separator />
+        <p className=" mt-2">
+          Don't have an account?{" "}
+          <Link to="/signup" className="text-blue-500 hover:underline">
+            Sign up
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
