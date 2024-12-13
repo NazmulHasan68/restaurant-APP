8bd4950ca0f44a5575b063e241d096a05ccec525
NazmulHasan68 <aishadowzone@gmail.com>
2024-12-13 15:26:03 +0600

    Stepup fontend with shadcn ui

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <Button variant="destructive">Destructive</Button>
+  )
+}
+
+export default App
