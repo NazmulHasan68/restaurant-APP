4b911a6cca41f25d8ab5bc9f848fc2a0b5136708
NazmulHasan68 <aishadowzone@gmail.com>
2024-12-24 20:56:57 +0600

    fix error

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-  theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {
-			orange : "var(--button)",
-			hoverOrange: "var(--hoverButtonColor)"
-		}
-  	}
-  },
-  plugins: [require("tailwindcss-animate")],
-}
+export default {
+	content: [
+	  "./src/**/*.{html,js,jsx,ts,tsx}",
+	  "./public/index.html",
+	],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
+  };
